2d4125cef0c138c1278aa2cd94484c4f61e00d1a
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-07-13 00:00:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const box = document.getElementById("box")
+
+document.addEventListener("click", function(){
+    box.style.backgroundColor = "yellow" 
+    box.style.height = "600px"
+    box.style.width = "600px"
+  });
